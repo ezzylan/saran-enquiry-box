491f310d10cc75f5e33b435a70ce3ccd0df3b025
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2024-07-14 01:59:11 +0800

    feat: setup enquiry form

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,67 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import * as z from 'zod'
+import isMobilePhone from 'validator/lib/isMobilePhone'
+import { useFetch } from '@vueuse/core'
+
+import { Button } from '@/components/ui/button'
+import { AutoForm } from '@/components/ui/auto-form'
+import { useToast } from '@/components/ui/toast/use-toast'
+import { Toaster } from '@/components/ui/toast'
+
+const { toast } = useToast()
+
+const formSchema = z.object({
+  name: z.string().min(3, {
+    message: 'Name must be at least 3 characters.'
+  }),
+  mobileNumber: z.string().min(9).max(10).refine(isMobilePhone),
+  email: z.string().email({ message: 'Invalid email address' }),
+  message: z.string().describe('Type your message')
+})
+
+function onSubmit(values: Record<string, any>) {
+  let telegramBotUrl = `https://api.telegram.org/bot${import.meta.env.VITE_TELEGRAM_BOT_TOKEN}/sendMessage`
+
+  let bodyContent = new FormData()
+  bodyContent.append('chat_id', '@saranenquiriestest')
+  bodyContent.append(
+    'text',
+    `
+    New enquiry!
+    Name: ${values.name}
+    Mobile Number: ${values.mobileNumber}
+    Email: ${values.email}
+    Message: ${values.message}
+    `
+  )
+
+  const { error } = useFetch(telegramBotUrl).post(bodyContent)
+
+  if (error.value)
+    toast({
+      title: 'Uh oh! Something went wrong.',
+      description: 'There was a problem with your request.',
+      variant: 'destructive'
+    })
+  else {
+    toast({
+      description: 'Your message has been sent successfully!'
+    })
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="container mx-auto p-8">
+    <AutoForm
+      class="space-y-6"
+      :schema="formSchema"
+      :field-config="{ message: { component: 'textarea' } }"
+      @submit="onSubmit"
+    >
+      <Button type="submit">Submit</Button>
+    </AutoForm>
   </main>
+
+  <Toaster />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
